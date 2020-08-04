5b77a2c281251dd99929c5acae023a457ff80bf7
rperoza <rodrigo.peroza@videplast.com.br>
2020-08-04 08:49:01 -0300

    Adicionar arquivos de projeto.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+
+        public ICollection<ItemPedido> ItensPedidos { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
