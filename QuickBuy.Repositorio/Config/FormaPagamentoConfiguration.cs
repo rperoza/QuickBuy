0b20a5e78f51375ff56a44977ae51f51dff72094
rperoza <rodrigo.peroza@videplast.com.br>
2020-08-20 16:49:47 -0300

    Configurando DbContext

---
+++ QuickBuy.Repositorio/Config/FormaPagamentoConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.ObjetoDeValor;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class FormaPagamentoConfiguration : IEntityTypeConfiguration<FormaPagamento>
+    {
+        public void Configure(EntityTypeBuilder<FormaPagamento> builder)
+        {
+        }
+    }
+}
